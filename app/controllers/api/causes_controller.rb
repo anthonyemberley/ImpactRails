5479762bd82920ac1d82f40283aeee512e379410
Anthony Emberley <emberley@mit.edu>
2015-09-03 15:55:06 -0400

    cause query by category

--- app/controllers/api/causes_controller.rb
+++ app/controllers/api/causes_controller.rb
@@ -17,23 +17,35 @@ class Api::CausesController < Api::ApiController
 		render_cause_index_response(@causes)
 	end
 
+	def get_causes_from_category
+		response = CategoryCausesService.new(causes_from_category_params).perform
+		if response.success?
+			causes = response.result
+			render_default_cause_response(causes)
+		else
+			render_error(404,response.errors)
+		end
+	end
 
 	private
-
-
 	    '''PARAMS '''
 	    def create_cause_params 
 	     	params.require(CAUSE_RESPONSE_KEY).permit(:name,:description, :category)
 	    end
 
-	    def render_cause_index_response(causes)
-	    	render status: :ok , json: causes.as_json
+
+		def causes_from_category_params
+			params.require(:category)
 		end
 
 	    '''RENDER'''
 	    def render_default_cause_response(cause)
 			render status: :ok , json: cause.as_json
 			
+		end
+
+		def render_cause_index_response(causes)
+	    	render status: :ok , json: causes.as_json
 		end
 
 	    def render_error(status,errors) 
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       post "/logout" => "sessions#logout"
       post "/facebook_auth" => "sessions#facebook_authentication"
       post "/create_cause" => "causes#create"
+      get "/causes/category" => "causes#get_causes_from_category"
       get "/current_user" => "sessions#get_current_user"
       get "/causes/all"  => "causes#index"
    end
