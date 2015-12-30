02db0b103634bce5ee800b78021d357c69972881
Anthony Emberley <emberley@mit.edu>
2015-12-30 12:39:40 -0700

    testing dates

--- app/services/user/update_weekly_budget_service.rb
+++ app/services/user/update_weekly_budget_service.rb
@@ -18,6 +18,20 @@ class UpdateWeeklyBudgetService < Aldous::Service
 
 	def update_user
 		@user.update_attribute(:weekly_budget, @value.to_f)
+		last_date_string = @user.last_contribution_date.to_formatted_s(:db)
+		now_date_string = Time.now.to_formatted_s(:db)
+		puts "halfway through"
+		puts "part of date string " + last_date_string[0,4]
+		puts "date_string " + last_date_string
+		puts "integer part of date string" + Integer(last_date_string[0,4]).to_s
+		next_year = Integer(last_date_string[0,4]) < Integer(now_date_string[0,4])
+		puts "year works"
+		puts "month string " + last_date_string[5] + "month string 2 " + now_date_string[5]
+		next_month = Integer(last_date_string[5]) < Integer(now_date_string[5])
+		puts "month works"
+		puts "date " + last_date_string[6] + "date 2" + now_date_string[6]
+		next_day = Integer(last_date_string[6]) < Integer(now_date_string[6])
+		puts "finished next year stuff"
 	end
 
 end
