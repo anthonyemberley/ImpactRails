6c6cf8b545a55a5c82ddaf67b9107ce74168db46
Phillip Ou <pou@mit.edu>
2016-01-30 12:36:19 -0800

    improved error handling and logging for credit card retrieval

--- app/services/stripe/get_stripe_cards_service.rb
+++ app/services/stripe/get_stripe_cards_service.rb
@@ -6,8 +6,14 @@ class GetStripeCardsService < Aldous::Service
 	end
 
 	def perform
+		puts "retrieving credit card information from with stripe_customer_id: "+@user.stripe_customer_id
+		if @user.stripe_customer_id.blank?
+			Result::Failure.new(errors: "You do not have a registered credit card")
+			return
+		end
 		begin 
 			card = Stripe::Customer.retrieve(@user.stripe_customer_id).sources.data
+			puts "retrieved "+card.to_s
 			Result::Success.new(result: card)
 		rescue Exception => errors
 			Result::Failure.new(errors: errors.message)
