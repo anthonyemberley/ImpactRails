2a8622421822818a7b3cf8a2653fc5ba8237e2ea
Phillip Ou <pou@mit.edu>
2016-02-02 12:52:33 -0500

    added delete card service

--- app/controllers/api/stripe_cards_controller.rb
+++ app/controllers/api/stripe_cards_controller.rb
@@ -8,4 +8,14 @@ class Api::StripeCardsController < Api::ApiController
 			render_error(:unauthorized, response.errors)
 		end
 	end
+
+	def delete_card
+		stripe_card_id = params[:card][:stripe_card_id]
+		response = DeleteStripeCardService.new(@current_user,stripe_card_id).perform
+		if response.succes?
+			render status: :ok , json: response.result.as_json
+		else
+			render_error(:unauthorized, response.errors)
+		end
+	end
 end
