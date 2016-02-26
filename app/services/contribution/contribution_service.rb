31a9e8fbee52847f17588ac3731a8da34a4169f1
Anthony Emberley <emberley@mit.edu>
2016-02-26 12:39:20 +0100

    debugggg

--- app/services/contribution/contribution_service.rb
+++ app/services/contribution/contribution_service.rb
@@ -12,11 +12,11 @@ class ContributionService < Aldous::Service
 			puts "@user.current_payment_id is nil"
 			Result::Failure.new(errors: "User does not have a current payment")
 		end
-		puts "amount:" + @amount,
-	    puts "user_id:" + @user.id,
-		puts "user_name: " + @user.name,
-		puts "cause_id: " +@cause.id,
-		puts "cause_name: " +@cause.name,
+		puts "amount:" + @amount
+	    puts "user_id:" + @user.id
+		puts "user_name: " + @user.name
+		puts "cause_id: " +@cause.id
+		puts "cause_name: " +@cause.name
 		puts "payment_id: " +@user.current_payment_id
 
 		contribution = Contribution.create(
