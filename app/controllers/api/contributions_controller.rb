2e8182f32de084f6827210ccc27ac70287943094
Anthony Emberley <emberley@mit.edu>
2016-03-02 20:42:03 +0100

    testing flat donation

--- app/controllers/api/contributions_controller.rb
+++ app/controllers/api/contributions_controller.rb
@@ -14,71 +14,71 @@ class Api::ContributionsController < Api::ApiController
 
 	def flat_donation
 
-		# '''Check if user has current active payment, create one if not'''
-		# no_current_payment = @current_user.current_payment_id.nil?
-		# if no_current_payment
-		# 	payment_response = CreatePaymentService.new(@current_user).perform
-		# 	if payment_response.failure?
-		# 		render_error(:bad_request,payment_response.errors)
-		# 	end
-		# end
+		'''Check if user has current active payment, create one if not'''
+		no_current_payment = @current_user.current_payment_id.nil?
+		if no_current_payment
+			payment_response = CreatePaymentService.new(@current_user).perform
+			if payment_response.failure?
+				render_error(:bad_request,payment_response.errors)
+			end
+		end
 
-		# '''Save Contribution '''
-		# amount = params[:contribution][:amount]#@current_user.pending_contribution_amount
-		# causeID = params[:contribution][:cause_id]
+		'''Save Contribution '''
+		amount = params[:contribution][:amount]#@current_user.pending_contribution_amount
+		causeID = params[:contribution][:cause_id]
 
-		# if !can_make_flat_donation?(amount)
-		# 	return
-		# end
+		if !can_make_flat_donation?(amount)
+			return
+		end
 
-		# '''Deleted updating relationship cause we dont need one?'''
-		# if causeID == @current_user.current_cause_id
-		# 	contribution_response = ContributionService.new(amount,@current_user).perform #STUB!
-		# 	if contribution_response.failure? 
-		# 		render_error(:bad_request,contribution_response.errors)
-		# 		return
-		# 	end
-		# 	puts "SAVE CONTRIBUTION SUCCESSFUL"
-		# 	contribution = contribution_response.result
+		'''Deleted updating relationship cause we dont need one?'''
+		if causeID == @current_user.current_cause_id
+			contribution_response = ContributionService.new(amount,@current_user).perform #STUB!
+			if contribution_response.failure? 
+				render_error(:bad_request,contribution_response.errors)
+				return
+			end
+			puts "SAVE CONTRIBUTION SUCCESSFUL"
+			contribution = contribution_response.result
 
-		# 	'''Update User and Cause Relationship (amount contributed to cause) '''
-		# 	user_cause_response = UpdateUserCauseService.new(@current_user,contribution.amount).perform #STUB!
-		# 	if user_cause_response.failure?
-		# 		render_error(:bad_request,user_cause_response.errors)
-		# 		return
-		# 	end
+			'''Update User and Cause Relationship (amount contributed to cause) '''
+			user_cause_response = UpdateUserCauseService.new(@current_user,contribution.amount).perform #STUB!
+			if user_cause_response.failure?
+				render_error(:bad_request,user_cause_response.errors)
+				return
+			end
 			
-		# else
-		# 	flat_donation_response = FlatDonationService.new(amount,@current_user, causeID).perform #STUB!
-		# 	if flat_donation_response.failure? 
-		# 		render_error(:bad_request,flat_donation_response.errors)
-		# 		return
-		# 	end
-		# 	puts "SAVE CONTRIBUTION SUCCESSFUL"
-		# 	contribution = flat_donation_response.result
+		else
+			flat_donation_response = FlatDonationService.new(amount,@current_user, causeID).perform #STUB!
+			if flat_donation_response.failure? 
+				render_error(:bad_request,flat_donation_response.errors)
+				return
+			end
+			puts "SAVE CONTRIBUTION SUCCESSFUL"
+			contribution = flat_donation_response.result
 
-		# end
+		end
 
 
-		# '''Update User and its active payment '''
-		# user_payment_response = UpdateUserPaymentService.new(@current_user,contribution).perform #STUB!
-		# if user_payment_response.failure?
-		# 	render_error(:bad_request,user_payment_response.errors)
-		# 	return
-		# end
+		'''Update User and its active payment '''
+		user_payment_response = UpdateUserPaymentService.new(@current_user,contribution).perform #STUB!
+		if user_payment_response.failure?
+			render_error(:bad_request,user_payment_response.errors)
+			return
+		end
 
-		# '''Check if payment is above threshold, if it is then we create a stripe charge '''
-		# payment = user_payment_response.result
-		# if payment.amount > PAYMENT_THRESHOLD
-		# 	charge_response = CreateStripeChargeService.new(@current_user,payment).perform
-		# 	if charge_response.failure?
-		# 		render_error(:bad_request,charge_response.errors)
-		# 		return
-		# 	end
-		# 	render status: :ok , json: charge_response.result.as_json
-		# 	return
-		# end
-		# render status: :ok, json: contribution.as_json
+		'''Check if payment is above threshold, if it is then we create a stripe charge '''
+		payment = user_payment_response.result
+		if payment.amount > PAYMENT_THRESHOLD
+			charge_response = CreateStripeChargeService.new(@current_user,payment).perform
+			if charge_response.failure?
+				render_error(:bad_request,charge_response.errors)
+				return
+			end
+			render status: :ok , json: charge_response.result.as_json
+			return
+		end
+		render status: :ok, json: contribution.as_json
 
 	end
 
