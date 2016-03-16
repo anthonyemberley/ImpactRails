9e8b80f4891b98ed395a76d884ee1eac030e07e6
Anthony Emberley <emberley@mit.edu>
2016-03-16 14:40:30 +0100

    debugging remove card

--- app/services/stripe/delete_stripe_card_service.rb
+++ app/services/stripe/delete_stripe_card_service.rb
@@ -9,9 +9,14 @@ class DeleteStripeCardService < Aldous::Service
 	def perform
 		begin 
 			card = Stripe::Customer.retrieve(@user.stripe_customer_id).sources.data
-			if card.count == 1
+			puts "here1"
+			card_array = Array(card)
+			puts "here2"
+			if card_array.count == 1
+				puts "here 3"
 				@current_user.update_attribute(:has_credit_card,false)
 			end
+			puts "here4"
 			customer = Stripe::Customer.retrieve(@user.stripe_customer_id)
 			response = customer.sources.retrieve(@stripe_card_id).delete
 			Result::Success.new(result: response)
