b9ca714ad46c49ebc4c8000303a6e5379570468e
Anthony Emberley <emberley@mit.edu>
2016-03-16 18:47:03 +0100

    finished credit card check

--- app/services/stripe/delete_stripe_card_service.rb
+++ app/services/stripe/delete_stripe_card_service.rb
@@ -8,20 +8,16 @@ class DeleteStripeCardService < Aldous::Service
 
 	def perform
 		begin 
-			puts "here"
 			card = Stripe::Customer.retrieve(@user.stripe_customer_id).sources.data
-			puts "here1"
 			card_array = Array(card)
-			puts "here2"
 			if card_array.count == 1
-				puts "here 3"
 				@user.update_attribute(:has_credit_card,false)
 			end
-			puts "here4"
 			customer = Stripe::Customer.retrieve(@user.stripe_customer_id)
 			response = customer.sources.retrieve(@stripe_card_id).delete
 			Result::Success.new(result: response)
 		rescue Exception => errors
+			puts errors.message
 			Result::Failure.new(errors: errors.message)
 		end
 	end
